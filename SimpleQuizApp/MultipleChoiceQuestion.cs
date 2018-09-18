c9719b93f4c9f973a1abbd563a94842878188a16
jcooper4 <jcooper4@css.edu>
2018-09-18 10:45:44 -0500

    Added MultipleChoiceQuestion Class

---
+++ SimpleQuizApp/MultipleChoiceQuestion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SimpleQuizApp
+{
+    public class MultipleChoiceQuestion : Question
+    {
+        String[] possibleAnswers;
+        int answerIndex;
+
+        public MultipleChoiceQuestion(String questionText, String[] possibleAnswers, int answerIndex) : base(questionText)
+        {
+
+            this.possibleAnswers = possibleAnswers;
+            this.answerIndex = answerIndex;
+        }
+        public override string getAnswer()
+        {
+            return possibleAnswers[answerIndex];
+        }
+
+        public override bool checkAnswer(string answer)
+        {
+            return (answer.Equals(possibleAnswers[answerIndex]));
+        }
+
+
+        /**
+         * getQuestion()
+         * @return Adds newline after each possible answer
+         */
+
+        public override String getQuestion()
+        {
+            String fullQuestion = questionText + "\r\n";
+            foreach (String s in possibleAnswers)
+            {
+                fullQuestion += s + "\r\n";
+            }
+            return fullQuestion;
+        }
+    }
+}
